7576c4dfb17489b14e938fd399d0077068816d4a
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-03 20:06:41 +0600

    add base.html

--- shop/webapp/views/products.py
+++ shop/webapp/views/products.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from webapp.models import Product
+
+
+class ProductListView(ListView):
+    template_name = 'products/product_list_view.html'
+    model = Product
