88e2e187c8c557ee5fb3d69a7baf90b21dcf040f
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-06 14:46:43 +0600

    in OrderCreateView parse request info

--- shop/webapp/urls.py
+++ shop/webapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from webapp.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, \
-    AddProductBasketView, ProductBasketListView, ProductBasketDeleteView
+    AddProductBasketView, ProductBasketListView, ProductBasketDeleteView, OrderCreateView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list_view'),
@@ -12,4 +12,6 @@ urlpatterns = [
     path('product/<int:pk>/basket/add/', AddProductBasketView.as_view(), name='product_basket_add_view'),
     path('basket/', ProductBasketListView.as_view(), name='product_basket_list_view'),
     path('basket/<int:pk>/delete', ProductBasketDeleteView.as_view(), name='product_basket_delete_view'),
+    path('order/create', OrderCreateView.as_view(), name='order_create_view'),
+
 ]
--- shop/webapp/views/orders.py
+++ shop/webapp/views/orders.py
@@ -1,4 +1,33 @@
+from django.shortcuts import redirect
+from django.views import View
 
 
-class OrderCreateView:
-    pass
+class OrderCreateView(View):
+    def post(self, request, *args, **kwargs):
+        product_basket = self.request.POST.get('productbasket_list')
+        product_basket = product_basket.split('[')[1]
+        product_basket = product_basket.split(']')[0]
+        product_basket = product_basket.split(',')
+        a_list = []
+        for i in product_basket:
+            a_list.append(i.split('ProductBasket:')[1])
+        b_list = []
+        for i in a_list:
+            b_list.append(i.split('>')[0])
+        print(b_list)
+        dict_c = {}
+        for i in b_list:
+            a = i.split(':')[0].strip()
+            b = i.split(':')[1].strip()
+            dict_c[a] = b
+        print(dict_c)
+
+
+        context = {}
+        # for i, j in request.GET:
+        #     print(i)
+        #     print(j)
+        #     context['i'] = j
+        # print(context)
+
+        return redirect('product_list_view')
