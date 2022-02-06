25628aaf4e07e15e404a8d7aa1bdc0bfc2e2b91d
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-06 22:50:48 +0600

    renew dump and add regitration to admin.py

--- shop/webapp/admin.py
+++ shop/webapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from webapp.models import Product, ProductBasket
+from webapp.models import Product, ProductBasket, Order, OrderProduct
 
 admin.site.register(Product)
 admin.site.register(ProductBasket)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
