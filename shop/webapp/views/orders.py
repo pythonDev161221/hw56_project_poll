8f0306fe84a454f170da5018ccf1b0da1a15c00c
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-06 17:06:13 +0600

    del prints

--- shop/webapp/views/orders.py
+++ shop/webapp/views/orders.py
@@ -7,7 +7,6 @@ from webapp.models import Product, ProductBasket
 class OrderCreateView(View):
     def post(self, request, *args, **kwargs):
         product_basket = self.request.POST.get('productbasket_list')
-        print(product_basket)
         if product_basket == '<QuerySet []>':
             return redirect('product_basket_list_view')
         product_basket = product_basket.split('[')[1]
@@ -19,13 +18,11 @@ class OrderCreateView(View):
         b_list = []
         for i in a_list:
             b_list.append(i.split('>')[0])
-        print(b_list)
         dict_c = {}
         for i in b_list:
             a = i.split(':')[0].strip()
             b = i.split(':')[1].strip()
             dict_c[a] = b
-        print(dict_c)
         products = Product.objects.all()
         for product in products:
             for key, value in dict_c.items():
