494ce3f0576e14d72ec44e9fa324b07f885ffee8
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-06 22:23:02 +0600

    change OrderCreateView(View) to OrderCreateView(CreateView)

--- shop/webapp/views/orders.py
+++ shop/webapp/views/orders.py
@@ -1,12 +1,68 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.views import View
+from django.views.generic import CreateView
 
 from webapp.forms import OrderForm
 from webapp.models import Product, ProductBasket, Order, OrderProduct
 
 
-class OrderCreateView(View):
-    def post(self, request, *args, **kwargs):
+# class OrderCreateView(View):
+#     def post(self, request, *args, **kwargs):
+#         product_basket = self.request.POST.get('productbasket_list')
+#         if product_basket == '<QuerySet []>':
+#             return redirect('product_basket_list_view')
+#         product_basket = product_basket.split('[')[1]
+#         product_basket = product_basket.split(']')[0]
+#         product_basket = product_basket.split(',')
+#         a_list = []
+#         for i in product_basket:
+#             a_list.append(i.split('ProductBasket:')[1])
+#         b_list = []
+#         for i in a_list:
+#             b_list.append(i.split('>')[0])
+#         dict_c = {}
+#         for i in b_list:
+#             a = i.split(':')[0].strip()
+#             b = i.split(':')[1].strip()
+#             dict_c[a] = b
+#         products = Product.objects.all()
+#
+#         for product in products:
+#             for key, value in dict_c.items():
+#                 if product.product == key:
+#                     value = int(value)
+#                     if product.balance < value:
+#                         raise ValueError(f'В магазине нет столько товара. Вы хотите {value} а в наличии только {product.balance}')
+#         username = self.request.POST.get('username')
+#         address = self.request.POST.get('address')
+#         phone = self.request.POST.get('phone')
+#         if username == '' or address == '' or phone == '':
+#             raise ValueError
+#         order = Order(username=username, address=address, phone=phone)
+#         order.save()
+#         for product in products:
+#
+#             for key, value in dict_c.items():
+#                 if product.product == key:
+#                     order_product = OrderProduct()
+#                     order_product.product = product
+#                     order_product.order = order
+#                     value = int(value)
+#                     order_product.volume = value
+#                     product.balance -= value
+#                     order_product.save()
+#                     product.save()
+#         ProductBasket.objects.all().delete()
+#         return redirect('product_basket_list_view')
+
+
+class OrderCreateView(CreateView):
+    model = Order
+    form_class = OrderForm
+
+    def form_valid(self, form):
         product_basket = self.request.POST.get('productbasket_list')
         if product_basket == '<QuerySet []>':
             return redirect('product_basket_list_view')
@@ -31,14 +87,10 @@ class OrderCreateView(View):
                 if product.product == key:
                     value = int(value)
                     if product.balance < value:
-                        raise ValueError(f'В магазине нет столько товара. Вы хотите {value} а в наличии только {product.balance}')
-        username = self.request.POST.get('username')
-        address = self.request.POST.get('address')
-        phone = self.request.POST.get('phone')
-        if username == '' or address == '' or phone == '':
-            raise ValueError
-        order = Order(username=username, address=address, phone=phone)
-        order.save()
+                        raise ValueError(
+                            f'В магазине нет столько товара. Вы хотите {value} '
+                            f'а в наличии только {product.balance}')
+        order = form.save()
         for product in products:
 
             for key, value in dict_c.items():
@@ -52,4 +104,7 @@ class OrderCreateView(View):
                     order_product.save()
                     product.save()
         ProductBasket.objects.all().delete()
-        return redirect('product_basket_list_view')
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('product_basket_list_view')
