5b0155c5cb8d146538429b6c5cc040c231af598d
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-07 16:08:33 +0600

    добавлен и отсортирован список заказов в админке

--- shop/webapp/admin.py
+++ shop/webapp/admin.py
@@ -5,5 +5,21 @@ from webapp.models import Product, ProductBasket, Order, OrderProduct
 
 admin.site.register(Product)
 admin.site.register(ProductBasket)
-admin.site.register(Order)
+
 admin.site.register(OrderProduct)
+# class ArticleAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'title', 'author', 'created_at']
+#     list_filter = ['author']
+#     search_fields = ['title', 'content']
+#     fields = ['title', 'author', 'content', 'created_at', 'updated_at']
+#     readonly_fields = ['created_at', 'updated_at']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'username', 'phone', 'created_at']
+    search_fields = ['-created_at']
+    fields = ['username', 'address', 'phone', 'created_at']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Order, OrderAdmin)
