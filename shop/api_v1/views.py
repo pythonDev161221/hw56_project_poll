9fd26aadc4e07a2f0197c626d92848696c46f9fe
pythonDev161221 <adverbekreklamov@gmail.com>
2022-04-01 16:47:14 +0600

    add api permissions

--- shop/api_v1/views.py
+++ shop/api_v1/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS, IsAdminUser, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -16,13 +16,25 @@ from webapp.models import Product, Order
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsAdminUser)
+
+    def get_permissions(self):
+        if self.request.method in SAFE_METHODS:
+            return []
+        return super().get_permissions()
 
 
 class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsAdminUser)
+
+    def get_permissions(self):
+        if self.request.method in "POST":
+            return []
+        return super().get_permissions()
+
+
 
 
 @ensure_csrf_cookie
